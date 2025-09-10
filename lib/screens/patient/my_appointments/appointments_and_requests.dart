ef19d6fe07eb0d0e2a1340bd09c288b3cabba1d9
mkasi <mkasigiven09@gmail.com>
2025-09-10 12:53:34 +0200

    hospital colors

---
+++ lib/screens/patient/my_appointments/appointments_and_requests.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+import 'appointments_tab.dart';
+import 'service_requests_tab.dart';
+
+class AppointmentsAndRequestsScreen extends StatelessWidget {
+  const AppointmentsAndRequestsScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return DefaultTabController(
+      length: 2,
+      child: Scaffold(
+        appBar: AppBar(
+          title: const Text('Appointments / Requests'),
+          bottom: const TabBar(
+            tabs: [
+              Tab(text: 'Appointments'),
+              Tab(text: 'Service Requests'),
+            ],
+          ),
+        ),
+        body: const TabBarView(
+          children: [
+            AppointmentsTab(),
+            ServiceRequestsTab(),
+          ],
+        ),
+      ),
+    );
+  }
+}
